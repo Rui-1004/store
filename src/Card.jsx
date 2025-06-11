9186f0f8866340fe7a1d7b12086c645e552c77f6
rui <77292122+Rui-1004@users.noreply.github.com>
2025-06-11 19:44:25 +0100

    Change Card input type Add handlers for quantity Add quantity change validation Fix cart icon quantity display

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import styles from "./Card.module.css";
 
 export default function Card({product}) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  }
+
+  const handleDecrement = () => {
+    setQuantity(quantity - 1);
+  }
+
+  const handleChange = (e) => {
+    let number = parseInt(e.target.value, 10);
+
+    if (isNaN(number) || number < 1) return;
+
+    setQuantity(number);
+  }
+
   return(
     <div className={styles.card}>
       <div className={styles.imageWrapper}>
@@ -12,9 +31,9 @@ export default function Card({product}) {
       </div>
       <div className={styles.cardInputs}>
         <div className={styles.quantityInput}>
-          <button className={styles.quantityMinus}>-</button>
-          <input name="quantity" type="text" />
-          <button className={styles.quantityPlus}>+</button>
+          <button className={styles.quantityMinus} onClick={handleDecrement} disabled={quantity === 1}>-</button>
+          <input name="quantity" type="number" onChange={handleChange} value={quantity} min={1} step={1} />
+          <button className={styles.quantityPlus} onClick={handleIncrement}>+</button>
         </div>
         <button className={styles.addToCart}>Add to Cart</button>
       </div>
